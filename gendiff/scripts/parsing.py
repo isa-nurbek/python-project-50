454ca947c3409c43453bbd99e5006b7a520c1bcc
isa-nurbek <isradin.110593@gmail.com>
2025-05-22 06:20:02 +0300

    Update file

--- gendiff/scripts/parsing.py
+++ gendiff/scripts/parsing.py
@@ -4,16 +4,17 @@ import os
 import yaml
 
 
-def parse_file(file_path):
-    _, ext = os.path.splitext(file_path)
-    with open(file_path, "r") as f:
-        if ext == ".json":
-            return json.load(f)
-        else:
-            return yaml.safe_load(f)
-
-
 def parse_files(file_path1, file_path2):
-    file1 = parse_file(file_path1)
-    file2 = parse_file(file_path2)
+    _, ext = os.path.splitext(file_path1)
+    if ext == ".json":
+        with open(file_path1, "r") as f1:
+            file1 = json.load(f1)
+        with open(file_path2, "r") as f2:
+            file2 = json.load(f2)
+    else:
+        with open(file_path1, "r") as f1:
+            file1 = yaml.safe_load(f1)
+        with open(file_path2, "r") as f2:
+            file2 = yaml.safe_load(f2)
+
     return file1, file2
