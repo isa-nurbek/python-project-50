d27a1f6d04ed6e7de010416dd8a2477ea2f4e65a
isa-nurbek <isradin.110593@gmail.com>
2025-05-22 05:42:07 +0300

    Update main.py

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -1,6 +1,10 @@
-#!/usr/bin/env python3
+from gendiff.cli import parse_cli
+from gendiff.scripts.gendiff import generate_diff
 
-from gendiff.scripts.gendiff import main
+
+def main():
+    file_path1, file_path2, format_name = parse_cli()
+    print(generate_diff(file_path1, file_path2, format_name))
 
 
 if __name__ == "__main__":
